ceb0f7284f4b62e3aacb2e51a87ff6fb53cbd1a8
Vindex <andres_ricardo1995@hotmail.com>
2020-09-16 11:19:37 -0500

    por  dañas kevin

---
+++ vinculacion/Assets/Activator.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour {
+
+    SpriteRenderer sr;
+    public KeyCode key;
+    bool active = false;
+    GameObject note;
+    Color old;
+    public bool createMOde;
+    public GameObject n;
+
+    // Use this for initialization
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+    void Start () {
+        old = sr.color;
+	}
+   
+
+    // Update is called once per frame
+    void Update () {
+        
+        if (createMOde)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                
+                Instantiate(n, transform.position, Quaternion.identity);
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(key))
+            {
+                StartCoroutine(Pressed());
+            }
+            if (Input.GetKeyDown(key) && active)
+            {
+                Destroy(note);
+                AddScore();
+                active = false;
+            }
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        active = true;
+        if (col.gameObject.tag == "Note")
+        {
+            note = col.gameObject;
+        }
+    }
+
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        active = false;  
+    }
+
+    void AddScore()
+    {
+        PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score") + 100);
+    }
+
+    IEnumerator Pressed()
+    {
+        Color old = GetComponent<SpriteRenderer>().color;
+        sr.color = new Color(0, 0, 0);
+        yield return new WaitForSeconds(0.05f);
+        sr.color = old;
+    }
+
+}
---
+++ vinculacion/Assets/over.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class over : MonoBehaviour {
+    public Text game;
+    public GameObject ever;
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "End")
+        {
+            game.text = "GameOver";
+            SceneManager.LoadScene("gameOver");
+        }
+    }
+
+    public void GAME()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void GAME1()
+    {
+        SceneManager.LoadScene("inicio");
+        PlayerPrefs.DeleteAll();
+    }
+}
